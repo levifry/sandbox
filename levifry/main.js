85839deb76665caf72bbcbc12d1eb7dce2afed6d
Levi Fry <levifry@Levis-MacBook-Air.local>
2022-04-25 22:47:52 -0700

    reload script

--- levifry/main.js
+++ levifry/main.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
   
   //let's code!
 
+  function reloadPage() {
+    location.reload();
+  }
+
   console.log('end main.js execution');
-  
+
 })
