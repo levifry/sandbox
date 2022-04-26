533cd7abf50d7da145e0387597e8e990cdaa6170
Levi Fry <levifry@Levis-MacBook-Air.local>
2022-04-25 23:17:17 -0700

    bugfix

--- levifry/main.js
+++ levifry/main.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
   
   //let's code!
 
-  function reloadPage() {
-    window.location.reload(true);
-  }
-  alert("Hello! I am an alert box!!");
-  console.log('end main.js script execution');
-
 });
+
+function reloadPage() {
+  window.location.reload(true);
+}
+
+console.log('end main.js script execution');
