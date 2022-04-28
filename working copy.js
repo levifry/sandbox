74401400cdc7622bd5a6e228f885ffc3e6c31de1
Levi Fry <levifry@Levis-MacBook-Air.local>
2022-04-27 23:52:48 -0700

    removed

--- working copy.js
+++
@@ -1,9 +0,0 @@
-["a string", ["array", "or", "object"]].forEach(arg => console.log(typeof arg));
-
-const arr = [1, 2, 3, 4];
-
-const odds = arr.filter(function(element) {
-  return element % 2 !== 0;
-});
-
-console.log(odds); // [1, 3]
